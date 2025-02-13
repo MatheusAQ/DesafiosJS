a75683bc765226aff604c0764442962902633757
Matheus <112505460+MatheusAQ@users.noreply.github.com>
2025-02-13 13:31:46 -0300

    Update ex2jogoadvinha.js

--- Desafiosjsbasico/ex2jogoadvinha.js
+++ Desafiosjsbasico/ex2jogoadvinha.js
@@ -1,4 +1,15 @@
+/*
+Desafio 3 - Jogo de Adivinhação
+Crie um programa que simula um jogo de adivinhação. O programa deve gerar um número aleatório entre 1 e 100 e permitir que o usuário tente adivinhar o número. O programa deve informar se o palpite é maior ou menor que o número secreto até que o usuário acerte.
 
+Regras:
+
+O número secreto deve ser gerado aleatoriamente entre 1 e 100.
+O usuário pode fazer quantas tentativas quiser até acertar.
+Após cada tentativa, o programa deve exibir se o número é maior ou menor que o palpite.
+Quando o usuário acertar, o programa deve exibir uma mensagem de parabéns e o número de tentativas usadas.
+
+*/
 // Usei o console do navegador
 
 let aleatorio = 0
