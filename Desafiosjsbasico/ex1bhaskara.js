8f000058cb8759bf6b9e72cb7ab59baefdf9a470
Matheus <112505460+MatheusAQ@users.noreply.github.com>
2025-02-13 13:29:59 -0300

    Update ex1bhaskara.js

--- Desafiosjsbasico/ex1bhaskara.js
+++ Desafiosjsbasico/ex1bhaskara.js
@@ -1,3 +1,9 @@
+/*
+Desafio 1 - Bhaskara
+Faça um programa em Javascript para calcular os valores x1 e x2 da fórmula de Bhaskara, usando as variáveis a, b e c, e mostre os valores x1 e x2. Usando os operadores de atribuição e aritméticos.
+*/
+
+
 let a = -1
 let b = 2
 let c = 3
