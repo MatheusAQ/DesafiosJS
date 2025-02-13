5d87c658f77384cd2ac18ca13eb74e86d72e265d
Matheus <112505460+MatheusAQ@users.noreply.github.com>
2025-02-13 13:30:42 -0300

    Update ex2bhaskara.js

--- Desafiosjsbasico/ex2bhaskara.js
+++ Desafiosjsbasico/ex2bhaskara.js
@@ -1,3 +1,12 @@
+/*
+Desafio 2 - Melhorar Programa Bhaskara
+Melhore o programa de Bhaskara do desafio anterior, adicionando IFs para verificar se:
+a) A variável “a” é válida (não pode ser zero);
+b) Se não existem raízes reais, e informar;
+c) Se existe apenas uma raiz real, e informar essa raiz;
+d) Se existem duas raízes reais, e as informar
+*/
+
 let a = -1
 let b = 1
 let c = 6
